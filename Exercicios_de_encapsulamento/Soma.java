c0b8f777a9e5531a9b75fe9d713cb0d92e398f86
CaioQC <129068301+CaioQC@users.noreply.github.com>
2024-09-05 10:45:50 -0300

    Update Soma.java
    Adicionando conteúdo da classe Soma

--- Exercicios_de_encapsulamento/Soma.java
+++ Exercicios_de_encapsulamento/Soma.java
@@ -1,3 +1,13 @@
 public class Soma {
+    public double soma(double a, double b){
+        return a + b;
+    }
 
+    public int soma(int a, int b){
+        return a + b;
+    }
+
+    public float soma(float a, float b){
+        return a + b;
+    }
 }
