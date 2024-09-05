e165dad9cc9cd987f272ecdf14f0bf374464bef3
CaioQC <129068301+CaioQC@users.noreply.github.com>
2024-09-05 11:06:35 -0300

    Update Produto.java
    Adicionando conteúdo da classe Produto

--- Exercicios_de_encapsulamento/Produto.java
+++ Exercicios_de_encapsulamento/Produto.java
@@ -1,1 +1,44 @@
+public class Produto {
+    public String nome;
+    public String codigo;
+    public double preco;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void exibirProduto(String nome){
+        System.out.println("O nome do produto é: " + nome);
+    }
+
+    public void exibirProduto(String nome, String codigo){
+        System.out.println("O nome do produto é: " + nome);
+        System.out.println("O codigo do produto é: " + codigo);
+    }
+
+    public void exibirProduto(String nome, String codigo, double preco){
+        System.out.println("O nome do produto é: " + nome);
+        System.out.println("O codigo do produto é: " + codigo);
+        System.out.println("O preço do produto é: " + preco);
+    }
+}
