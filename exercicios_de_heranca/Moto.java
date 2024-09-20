81c2b90e704fb81b26e7625ccb722517aca3660f
CaioQC <129068301+CaioQC@users.noreply.github.com>
2024-09-19 22:36:29 -0300

    Update Moto.java
    Adicionando versão final da Classe Moto

--- exercicios_de_heranca/Moto.java
+++ exercicios_de_heranca/Moto.java
@@ -1,4 +1,20 @@
 package Veiculos;
 
-public class Moto {
+public class Moto extends Veiculo{
+    private boolean temCarenagem;
+
+    public Moto(String marca, String modelo, int ano, boolean temCarenagem) {
+        super(marca, modelo, ano);
+        this.temCarenagem = temCarenagem;
+    }
+
+    public void empinar(){
+        System.out.println("Empinando.");
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Tem carenagem: " + temCarenagem);
+    }
 }
